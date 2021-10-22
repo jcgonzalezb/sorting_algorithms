e5daa518f55c161ed23d4e096322c889e269c0c5
Mapu456 <mpaula456@hotmail.com>
2021-10-22 10:21:01 -0500

    up

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,32 +7,40 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp1;
+	listint_t *temp1, *temp2;
 	int size = 0;
-	int i;
 
 	if (list == NULL || *list == NULL)
 	{
 		return;
 	}
-	while (list != NULL)
+	temp1 = (*list)->next;
+	while (temp1 != NULL)
 	{
-		*list = (*list)->next;
-		size++;
-	}
-	for (i = 0; i < size; i++)
-	{
-		temp1 = *list;
-		while (temp1->next && temp1->n > temp1->next->n)
-		{
-			temp1->prev->next = temp1->next;
-			temp1->next->next->prev = temp1->next->prev;
-			temp1->next->prev = temp1->prev;
-			temp1->prev = temp1->next;
-			temp1->next = temp1->next->next;
-			temp1->next->next = temp1;
-		}
+		temp2 = temp1;
 		temp1 = temp1->next;
-		print_list(*list);
+		size++;
+		while (temp2->prev)
+		{
+			if (temp2->prev->n > temp2->n)
+			{
+				/* swap nodes*/
+				temp2->prev->next = temp2->next;
+				if (temp2->next != NULL)
+					temp2->next->prev = temp2->prev;
+				temp2->next = temp2->prev;
+				temp2->prev = temp2->next->prev;
+				if (temp2->prev != NULL)
+					temp2->prev->next = temp2;
+				else
+					*list = temp2;
+				temp2->next->prev = temp2;
+				print_list(*list);
+			}
+			else
+			{
+				break;
+			}
+		}
 	}
 }
