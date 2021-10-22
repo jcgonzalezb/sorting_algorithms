a3c5bfed6757a0f926bfba2d3018f4578ac67489
Mapu456 <mpaula456@hotmail.com>
2021-10-22 18:14:21 -0500

    point 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,56 +1,69 @@
 #include "sort.h"
-// A utility function to swap two elements
+/**
+ * quick_sort - function that sorts a doubly linked list of integers
+ * @array: array
+ * @size: array
+ * Return: the value
+ */
 void quick_sort(int *array, size_t size)
 {
-	quickSort(array, 0, size - 1);
+	quickSort(array, 0, size - 1, size);
 }
-void swap(int* a, int* b)
+/**
+ * swap - function that sorts a doubly linked list of integers
+ * @a: array
+ * @b: array
+ * Return: the value
+ */
+void swap(int *a, int *b)
 {
 	int t = *a;
 	*a = *b;
 	*b = t;
 }
-/* This function takes last element as pivot, places
-the pivot element at its correct position in sorted
-arrayay, and places all smaller (smaller than pivot)
-to left of pivot and all greater elements to right
-of pivot */
-int partition (int array[], int low, int high)
+/**
+ * partition - function that sorts a doubly linked list of integers
+ * @array: array
+ * @low: array
+ * @high: array
+ * @size: array
+ * Return: the value
+ */
+int partition(int array[], int low, int high, size_t size)
 {
-	int pivot = array[high]; // pivot
-	int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
+	int pivot = array[high];
+	int i = (low - 1);
 
 	for (int j = low; j <= high - 1; j++)
 	{
-		// If current element is smaller than the pivot
 		if (array[j] < pivot)
 		{
-			i++; // increment index of smaller element
+			i++;
 			swap(&array[i], &array[j]);
 			if (i != j)
-				print_array(array, 10);
+				print_array(array, size);
 		}
 	}
 	swap(&array[i + 1], &array[high]);
 	if (i + 1 != high)
-		print_array(array, 10);
+		print_array(array, size);
 	return (i + 1);
 }
-/* The main function that implements QuickSort
-array[] --> arrayay to be sorted,
-low --> Starting index,
-high --> Ending index */
-void quickSort(int array[], int low, int high)
+/**
+ * quickSort - function that sorts a doubly linked list of integers
+ * @array: array
+ * @low: array
+ * @high: array
+ * @size: array
+ * Return: the value
+ */
+void quickSort(int array[], int low, int high, size_t size)
 {
 	if (low < high)
 	{
-		/* pi is partitioning index, array[p] is now
-		at right place */
-		int pi = partition(array, low, high);
+		int pi = partition(array, low, high, size);
 
-		// Separately sort elements before
-		// partition and after partition
-		quickSort(array, low, pi - 1);
-		quickSort(array, pi + 1, high);
+		quickSort(array, low, pi - 1, size);
+		quickSort(array, pi + 1, high, size);
 	}
 }
