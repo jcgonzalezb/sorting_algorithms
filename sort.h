c762932df84741ac546b7da0ba80a6f360832120
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-25 18:32:26 -0500

    sort file

--- sort.h
+++ sort.h
@@ -28,5 +28,7 @@ void print_list(const listint_t *list);
 void quickSort(int *array, int low, int high, size_t size);
 int partition(int *array, int low, int high, size_t size);
 void swap(int *a, int *b);
+void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif
