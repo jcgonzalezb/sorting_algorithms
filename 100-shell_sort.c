ce1fb75048d08f7e78b671611ec39a8e0fb463f2
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-26 11:44:34 -0500

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,7 +11,7 @@ void shell_sort(int *array, size_t size)
 {
 	int i = 0;
 	int j = 0;
-	int interval = 1;
+	int gap = 1;
 	int max = size;
 	int temp = 0;
 	int first = array[0];
@@ -19,23 +19,23 @@ void shell_sort(int *array, size_t size)
 	if (!array || size < 2 || !size)
 		return;
 
-	while (interval < max / 3)
+	while (gap < max / 3)
 	{
-		interval = interval * 3 + 1;
+		gap = gap * 3 + 1;
 	}
-	while (interval > 0)
+	while (gap > 0)
 	{
-		for (i = interval; i < max; i++)
+		for (i = gap; i < max; i++)
 		{
 			temp = array[i];
 
-			for (j = i; j > (interval - 1) && array[j - interval] >= temp; j -= interval)
+			for (j = i; j > (gap - 1) && array[j - gap] >= temp; j -= gap)
 			{
-				array[j] = array[j - interval];
+				array[j] = array[j - gap];
 			}
 			array[j] = temp;
 		}
-		interval = interval / 3;
+		gap = gap / 3;
 		if (first != array[j])
 			print_array(array, size);
 	}
